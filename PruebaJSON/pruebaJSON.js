7e07ebb9459224987e3d79caabfc16cfd382bd38
Martin <martinlbernal@gmail.com>
2022-09-11 19:20:53 -0300

    subo pruebas JSON

---
+++ PruebaJSON/pruebaJSON.js
@@ -0,0 +1,24 @@
+var cocinerxs = [
+  {
+    nombre: "Martín",
+    apellido: "Bernal",
+    Edad: 33,
+    Cocina: true,
+    plato: {
+      nombre: "arroz",
+      porciones: 8,
+    },
+  },
+  {
+    nombre: "Celeste",
+    apellido: "Victorio",
+    Edad: 30,
+    Cocina: true,
+    plato: {
+      nombre: "fideos",
+      porciones: 4,
+    },
+  },
+];
+
+console.log(cocinerxs);
